d6ffd756f175d58876f0792ad20ea28173807e81
jongbeen <48251039+jongbeen@users.noreply.github.com>
2020-01-29 22:02:40 +0900

    List Collection(ArrayList/Vector/LinkedList)

---
+++ Grammer/src/Collections/Collections00_Why.java
@@ -0,0 +1,12 @@
+package Collections;
+
+public class Collections00_Why {
+	
+//	배열은 쉽게 생성하고 사용할 수 있지만, 불특정 다수의 객체를 저장하기엔 문제가 있음(듬성듬성)
+	
+//	Collection -> List (ArrayList, Vector, LinkedList)  		순서를 유지하고 저장, 중복저장 가능
+//	           -> Set (HashSet,TreeSet)                  		순서 유지 X, 중복 저장 X
+//	   Map	   -> HashMap/ Hashtable/ TreeMap/ Properties		<키,쌍>구조, 키 중복X
+	   
+
+}
---
+++ Grammer/src/Collections/Collections03_LinkedList.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+// ArrayList와 사용방법은 똑같지만 내부구조가 다르다
+// 공부했다시피, Linkedlist는 앞뒤로 Link가 존재해, 중간의 객체 삽입or삭제시,
+// 한번씩 모두 앞,뒤로 이동하는 ArrayList와 달리  중간 해당 index 앞,뒤 link만 교체하면 되므로 문제 X
+
+public class Collections03_LinkedList {
+	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<String>();
+		List<String> list2 = new LinkedList<String>();
+		
+		// 뭐 탐색시간 비교는 굳이 안하는걸로... 사용법 똑같고 생성하는법만 요렇게 다르다....
+	}
+}
